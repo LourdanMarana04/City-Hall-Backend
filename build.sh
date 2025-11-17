d955434f4c98ea5a47e1fb79414f1b38eb050c0e
jamesy <maranajames41@gmail.com>
2025-11-17 17:18:05 +0800

    City Hall Backend

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 install-php-extensions gd
-composer install --optimize-autoloader --no-dev --ignore-platform-req=ext-gd
+composer install --optimize-autoloader --no-dev --ignore-platform-reqs

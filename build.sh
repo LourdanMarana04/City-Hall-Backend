ca4ab16808c0ff990cf5d5949844e70a3f4e50db
jamesy <maranajames41@gmail.com>
2025-11-17 17:42:08 +0800

    City Hall Backend

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-install-php-extensions gd
+apt-get update && apt-get install -y libpng-dev libjpeg-dev libfreetype6-dev
+docker-php-ext-configure gd --with-freetype --with-jpeg
+docker-php-ext-install gd
 composer install --optimize-autoloader --no-dev --ignore-platform-reqs
